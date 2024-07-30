176188a8b6e8ec61b87166bf74f1e8b347a30a70
Joao Patricio <ijpatricio@gmail.com>
2024-07-30 14:15:55 +0100

    wip

--- resources/js/Sandbox/Sandbox.jsx
+++ resources/js/Sandbox/Sandbox.jsx
@@ -7,16 +7,16 @@ import { Toaster } from "@/components/ui/toaster"
 
 function App() {
     return (
-        <div className="container mx-auto p-4 h-screen flex flex-col">
-            <h1 className="text-xl font-bold mb-4">
-                Tinker with PHP in the browser. No server required.
+        <div className="mx-auto mt-2 h-screen flex flex-col">
+            <h1 className="text-xl font-bold pl-3 mb-4">
+                PHP Playground in the browser. No server required.
             </h1>
             <ResizablePanelGroup
                 direction="horizontal"
                 className="flex-grow rounded-lg border"
             >
                 <ResizablePanel defaultSize={25} minSize={20}>
-                    <div className="h-full p-1 bg-white overflow-auto">
+                    <div className="h-full pl-3 py-1 bg-white overflow-auto">
                         <h2 className="text-lg font-semibold mb-2">Project files</h2>
                         <EditorFolder path="/persist/01J3YR5QM15578K6YRACP6X3RA" name="/" />
                     </div>
@@ -25,7 +25,7 @@ function App() {
                 <ResizableHandle />
 
                 <ResizablePanel defaultSize={75}>
-                    <div className="h-full p-4 bg-white">
+                    <div className="h-full bg-white">
                         <CodeEditor />
                     </div>
                 </ResizablePanel>
