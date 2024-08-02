4cf8fe76244b148b7eb8054a8a934fdafd0ff65a
Joao Patricio <ijpatricio@gmail.com>
2024-08-02 08:21:59 +0100

    wip

--- app/Http/Controllers/SandboxController.php
+++ app/Http/Controllers/SandboxController.php
@@ -6,15 +6,21 @@ use App\Models\Sandbox;
 
 class SandboxController extends Controller
 {
-    public function show(Sandbox $sandbox)
+    public function show($sandbox)
     {
+        dd($sandbox);
+        $sandbox = Sandbox::whereUlid($sandbox)->firstOrFail();
+
         return view('sandbox.show', [
             'sandbox' => $sandbox,
         ]);
     }
 
-    public function preview(Sandbox $sandbox)
+    public function preview($sandbox)
     {
+//        dd($sandbox);
+        $sandbox = Sandbox::whereUlid(str($sandbox)->upper()->value())->firstOrFail();
+
         return view('sandbox.preview', [
             'sandbox' => $sandbox,
         ]);
