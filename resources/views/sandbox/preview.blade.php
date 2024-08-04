bc561e64c1f755cb41ab6c53ece9c118e91d84d4
Joao Patricio <ijpatricio@gmail.com>
2024-08-04 22:05:54 +0100

    Output information messages while progressing

--- resources/views/sandbox/preview.blade.php
+++ resources/views/sandbox/preview.blade.php
@@ -10,4 +10,15 @@
 
 @section('content')
     @vite(['resources/js/sandbox-preview/index.js'])
+    <div class="flex justify-center items-center h-screen">
+        <div id="message" class="text-lg">Starting sandbox...</div>
+    </div>
+    <script>
+        window.addEventListener('install-status', event => {
+            document.getElementById('message').innerHTML = event.detail;
+        });
+        window.addEventListener('install-complete', event => {
+            document.getElementById('message').innerHTML = 'Sandbox installed! Starting...';
+        });
+    </script>
 @endsection
